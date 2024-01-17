ff7f4c30ef0f2902b94cd067003a1c159624ca18
chelizichen <1347290221@qq.com>
2024-01-17 10:02:20 +0800

    feat:脚本优化

--- servers/CalcServer/build.sh
+++ servers/CalcServer/build.sh
@@ -2,12 +2,13 @@
 
 # if permission denied
 # run script with ` chmod +x build.sh ` 
+readonly ServerName="CalcServer"
 
 # rm
-rm ./CalcServer.tar.gz ./service_go
+rm ./$ServerName.tar.gz ./service_go
 
 # compile
 go build -o service_go
 
 # build
-tar -cvf CalcServer.tar.gz ./simp.yaml ./service_go ./web
+tar -cvf $ServerName.tar.gz ./simp.yaml ./service_go ./web
