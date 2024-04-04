ab34b5d224b84ed7e3488d6743be1625016abf6e
chelizichen <1347290221@qq.com>
2024-04-04 10:33:20 +0800

    feat:服务分层优化

--- client/src/components/aside.vue
+++ client/src/components/aside.vue
@@ -17,14 +17,17 @@ const keyword = ref("");
 const serverList = computed(() => {
   return props.serverList
     .filter((v) => v.match(keyword.value))
-    .reduce((pre: string[], curr: string) => {
-      if (curr.startsWith("Simp")) {
-        pre.unshift(curr);
-      } else {
-        pre.push(curr);
-      }
-      return pre;
-    }, []);
+    .reduce(
+      (pre: [string[], string[]], curr: string) => {
+        if (curr.startsWith("Simp")) {
+          pre[0].push(curr);
+        } else {
+          pre[1].push(curr);
+        }
+        return pre;
+      },
+      [[], []]
+    );
 });
 function toGit() {
   window.open("https://github.com/chelizichen/Simp");
@@ -41,28 +44,48 @@ function toGit() {
       class="el-menu-vertical-demo"
       active-text-color="rgb(207, 15, 124)"
       style="border: none"
+      :default-openeds="['2']"
     >
       <el-menu-item class="app-text-center">
         <el-icon class="app-not-show"><Search /></el-icon>
         <el-input v-model="keyword"></el-input>
       </el-menu-item>
-      <el-menu-item
-        v-for="(item, index) in serverList"
-        class="app-text-center"
-        :index="item"
-        :key="index"
-        @click="handleOpen(item)"
-      >
-        <el-icon class="app-not-show">
-          <template v-if="item.startsWith('Simp')">
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><Setting /></el-icon>
+          <span>Controller</span>
+        </template>
+        <el-menu-item
+          v-for="(item, index) in serverList[0]"
+          class="app-text-center"
+          :index="item"
+          :key="index"
+          @click="handleOpen(item)"
+        >
+          <el-icon class="app-not-show">
             <TrendCharts />
-          </template>
-          <template v-else>
+          </el-icon>
+          <template #title>{{ item }}</template>
+        </el-menu-item>
+      </el-sub-menu>
+      <el-sub-menu index="2">
+        <template #title>
+          <el-icon> <Operation /></el-icon>
+          <span>SubServer</span>
+        </template>
+        <el-menu-item
+          v-for="(item, index) in serverList[1]"
+          class="app-text-center"
+          :index="item"
+          :key="index"
+          @click="handleOpen(item)"
+        >
+          <el-icon class="app-not-show">
             <Menu />
-          </template>
-        </el-icon>
-        <template #title>{{ item }}</template>
-      </el-menu-item>
+          </el-icon>
+          <template #title>{{ item }}</template>
+        </el-menu-item>
+      </el-sub-menu>
     </el-menu>
   </div>
 </template>
